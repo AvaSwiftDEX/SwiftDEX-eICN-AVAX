36206a488c505014cd330edc818a8711892fd959
kimroniny <1301862177@qq.com>
2025-02-08 20:50:04 +0800

    extend ethclient

---
+++ ethclientExt/ethclientExt.go
@@ -0,0 +1,58 @@
+package ethclientext
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+type EthclientExt struct {
+	*ethclient.Client
+}
+
+func Dial(url string) (*EthclientExt, error) {
+	client, err := ethclient.Dial(url)
+	if err != nil {
+		return nil, err
+	}
+	return &EthclientExt{client}, nil
+}
+
+func (ecext *EthclientExt) Accounts(ctx context.Context) ([]common.Address, error) {
+	var result []common.Address
+	err := ecext.Client.Client().CallContext(ctx, &result, "eth_accounts")
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (ec *EthclientExt) SendWrapTransaction(ctx context.Context, tx *types.Transaction) error {
+	data, err := tx.MarshalBinary()
+	if err != nil {
+		return err
+	}
+	return ec.Client.Client().CallContext(ctx, nil, "eth_sendRawTransaction", hexutil.Encode(data))
+}
+
+func (ec *EthclientExt) WaitTransactionReceipt(ctx context.Context, txhash common.Hash, timeout time.Duration) (*types.Receipt, error) {
+	starttime := time.Now()
+	for {
+		txreceipt, err := ec.TransactionReceipt(ctx, txhash)
+		if txreceipt != nil {
+			fmt.Println("Transaction receipt received, height:", txreceipt.BlockNumber.Uint64(), "status:", txreceipt.Status)
+			return txreceipt, nil
+		}
+		if err != nil {
+			time.Sleep(500 * time.Millisecond)
+		}
+		if time.Since(starttime) > timeout {
+			return nil, fmt.Errorf("timeout waiting for transaction receipt")
+		}
+	}
+}
