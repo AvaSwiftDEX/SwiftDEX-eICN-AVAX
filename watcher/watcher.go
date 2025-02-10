a8506433b599e153269cf565a65f32a2acc538a5
kimroniny <1301862177@qq.com>
2025-02-10 16:11:51 +0800

    add watcher

---
+++ watcher/watcher.go
@@ -0,0 +1,183 @@
+package watcher
+
+import (
+	"context"
+	"encoding/hex"
+	"fmt"
+	"log"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/kimroniny/SuperRunner-eICN-eth2/SR2PC"
+	"github.com/kimroniny/SuperRunner-eICN-eth2/client"
+	ethclientext "github.com/kimroniny/SuperRunner-eICN-eth2/ethclientExt"
+)
+
+type SyncHeaderData struct {
+	Number *big.Int
+	Root   common.Hash
+}
+
+type CrossReceiveData struct {
+	Data1 []byte
+	Data2 []byte
+}
+
+type Watcher struct {
+	ctx               context.Context
+	httpUrl           string
+	wsUrl             string
+	chainId           *big.Int
+	address           common.Address
+	httpClient        *ethclientext.EthclientExt
+	wsClient          *ethclientext.EthclientExt
+	transmitterClient *client.ITransmitterClient
+	headerCh          chan *SyncHeaderData
+	crossReceiveCh    chan *CrossReceiveData
+}
+
+func NewWatcher(
+	ctx context.Context,
+	httpUrl string,
+	wsUrl string,
+	address common.Address,
+	chainId *big.Int,
+	transmitterClient *client.ITransmitterClient,
+) (*Watcher, error) {
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	wc := &Watcher{
+		ctx:               ctx,
+		httpUrl:           httpUrl,
+		wsUrl:             wsUrl,
+		address:           address,
+		chainId:           chainId,
+		transmitterClient: transmitterClient,
+		httpClient:        nil,
+		wsClient:          nil,
+		headerCh:          make(chan *SyncHeaderData, 1024),
+		crossReceiveCh:    make(chan *CrossReceiveData, 1024),
+	}
+	if httpUrl != "" {
+		client, err := ethclientext.Dial(httpUrl)
+		if err != nil {
+			return nil, err
+		}
+		wc.httpClient = client
+	}
+	if wsUrl != "" {
+		client, err := ethclientext.Dial(wsUrl)
+		if err != nil {
+			return nil, err
+		}
+		wc.wsClient = client
+	}
+	return wc, nil
+}
+
+func (wc *Watcher) MonitorBlock(ctx context.Context) {
+	if wc.wsClient == nil {
+		panic("ws client is nil")
+	}
+	if wc.httpClient == nil {
+		panic("http client is nil")
+	}
+	headers := make(chan *types.Header)
+	sub, err := wc.wsClient.SubscribeNewHead(context.Background(), headers)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case err := <-sub.Err():
+			log.Fatal(err)
+		case header := <-headers:
+			fmt.Println(header.Number.Uint64(), header.Hash().Hex()) // 0xbc10defa8dda384c96a17640d84de5578804945d347072e091b4e5f390ddea7f
+
+			header, err := wc.httpClient.HeaderByNumber(wc.ctx, header.Number)
+			if err != nil {
+				log.Fatal("get block by hash, while error: ", err)
+			}
+
+			fmt.Println(header.Hash().Hex())    // 0xbc10defa8dda384c96a17640d84de5578804945d347072e091b4e5f390ddea7f
+			fmt.Println(header.Number.Uint64()) // 3477413
+			fmt.Println(header.Time)            // 1529525947
+			fmt.Println(header.Nonce.Uint64())  // 130524141876765836
+
+			// 发送 header 到 headerCh
+			wc.headerCh <- &SyncHeaderData{
+				Number: header.Number,
+				Root:   header.Root,
+			}
+		}
+	}
+}
+
+func (wc *Watcher) SendHeader(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case header := <-wc.headerCh:
+			// 处理接收到的 header
+			fmt.Printf("Received new header from headerCh of MonitorBlock, number: %d, hash: %s\n",
+				header.Number.Uint64(),
+				header.Root.Hex())
+			err := (*wc.transmitterClient).SyncHeader(wc.chainId, header.Number, header.Root)
+			if err != nil {
+				log.Fatal("send header to transmitter client, while error: ", err)
+			}
+		}
+	}
+}
+
+func (wc *Watcher) MonitorEvent(ctx context.Context) {
+	if wc.httpClient == nil {
+		panic("http client is nil")
+	}
+	if wc.wsClient == nil {
+		panic("ws client is nil")
+	}
+	fmt.Println(">>> MonitorEvent")
+	instance, err := SR2PC.NewSR2PC(wc.address, wc.wsClient)
+	if err != nil {
+		panic(err)
+	}
+	logs := make(chan *SR2PC.SR2PCSendCMHash)
+	sub, err := instance.WatchSendCMHash(nil, logs)
+	if err != nil {
+		panic(err)
+	}
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case err := <-sub.Err():
+			panic(err)
+		case vLog := <-logs:
+			fmt.Println(">>>>>>>>>>>>>>>>>>>> find new log <<<<<<<<<<<<<<<<<<<<")
+			fmt.Println("cmHash: ", hex.EncodeToString(vLog.CmHash[:]))
+			fmt.Println("status: ", vLog.Status)
+		}
+	}
+}
+
+func (wc *Watcher) CrossReceive(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case data := <-wc.crossReceiveCh:
+			fmt.Println("crossReceive: ", data)
+			err := (*wc.transmitterClient).CrossReceive(data.Data1, data.Data2)
+			if err != nil {
+				log.Fatal("send crossReceive to transmitter client, while error: ", err)
+			}
+		}
+	}
+}
---
+++ watcher/watcher_test.go
@@ -0,0 +1,153 @@
+package watcher
+
+import (
+	"context"
+	"math/big"
+	"testing"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/kimroniny/SuperRunner-eICN-eth2/client"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+// 创建一个模拟的 TransmitterClient
+type MockTransmitterClient struct {
+	mock.Mock
+}
+
+func (m *MockTransmitterClient) SyncHeader(chainId *big.Int, number *big.Int, root common.Hash) error {
+	args := m.Called(chainId, number, root)
+	return args.Error(0)
+}
+
+func (m *MockTransmitterClient) CrossReceive(data1 []byte, data2 []byte) error {
+	args := m.Called(data1, data2)
+	return args.Error(0)
+}
+
+func (m *MockTransmitterClient) RegisterEICN(url string, chainID int) error {
+	args := m.Called(url, chainID)
+	return args.Error(0)
+}
+
+var _ client.ITransmitterClient = &MockTransmitterClient{}
+
+func TestNewWatcher(t *testing.T) {
+	// 准备测试数据
+	ctx := context.Background()
+	httpUrl := "http://localhost:8545"
+	wsUrl := "ws://localhost:8546"
+	address := common.HexToAddress("0x1234567890")
+	chainId := big.NewInt(1)
+	mockClient := new(MockTransmitterClient)
+	var transmitterClient client.ITransmitterClient = mockClient
+	// 测试创建 Watcher
+	watcher, err := NewWatcher(ctx, httpUrl, wsUrl, address, chainId, &transmitterClient)
+
+	// 验证结果
+	assert.NoError(t, err)
+	assert.NotNil(t, watcher)
+	assert.Equal(t, httpUrl, watcher.httpUrl)
+	assert.Equal(t, wsUrl, watcher.wsUrl)
+	assert.Equal(t, address, watcher.address)
+	assert.Equal(t, chainId, watcher.chainId)
+	assert.NotNil(t, watcher.headerCh)
+	assert.NotNil(t, watcher.crossReceiveCh)
+}
+
+func TestWatcher_SendHeader(t *testing.T) {
+	// 准备测试数据
+	ctx, cancel := context.WithCancel(context.Background())
+	mockClient := new(MockTransmitterClient)
+	var transmitterClient client.ITransmitterClient = mockClient
+	watcher, _ := NewWatcher(ctx, "", "", common.Address{}, big.NewInt(1), &transmitterClient)
+
+	// 设置 mock 期望
+	mockClient.On("SyncHeader", mock.Anything, mock.Anything, mock.Anything).Return(nil)
+
+	// 启动 SendHeader
+	go watcher.SendHeader(ctx)
+
+	// 发送测试数据
+	testHeader := &SyncHeaderData{
+		Number: big.NewInt(100),
+		Root:   common.HexToHash("0x1234"),
+	}
+	watcher.headerCh <- testHeader
+
+	// 等待处理完成
+	time.Sleep(100 * time.Millisecond)
+
+	// 验证 mock 是否被调用
+	mockClient.AssertCalled(t, "SyncHeader", mock.Anything, mock.Anything, mock.Anything)
+
+	// 清理
+	cancel()
+}
+
+func TestWatcher_CrossReceive(t *testing.T) {
+	// 准备测试数据
+	ctx, cancel := context.WithCancel(context.Background())
+	mockClient := new(MockTransmitterClient)
+	var transmitterClient client.ITransmitterClient = mockClient
+	watcher, _ := NewWatcher(ctx, "", "", common.Address{}, big.NewInt(1), &transmitterClient)
+
+	// 设置 mock 期望
+	mockClient.On("CrossReceive", mock.Anything, mock.Anything).Return(nil)
+
+	// 启动 CrossReceive
+	go watcher.CrossReceive(ctx)
+
+	// 发送测试数据
+	testData := &CrossReceiveData{
+		Data1: []byte("test1"),
+		Data2: []byte("test2"),
+	}
+	watcher.crossReceiveCh <- testData
+
+	// 等待处理完成
+	time.Sleep(100 * time.Millisecond)
+
+	// 验证 mock 是否被调用
+	mockClient.AssertCalled(t, "CrossReceive", mock.Anything, mock.Anything)
+
+	// 清理
+	cancel()
+}
+
+func TestWatcher_ContextCancellation(t *testing.T) {
+	// 准备测试数据
+	ctx, cancel := context.WithCancel(context.Background())
+	mockClient := new(MockTransmitterClient)
+	var transmitterClient client.ITransmitterClient = mockClient
+	watcher, _ := NewWatcher(ctx, "", "", common.Address{}, big.NewInt(1), &transmitterClient)
+
+	// 启动所有监听器
+	done := make(chan bool)
+	go func() {
+		watcher.SendHeader(ctx)
+		done <- true
+	}()
+
+	go func() {
+		watcher.CrossReceive(ctx)
+		done <- true
+	}()
+
+	// 取消 context
+	cancel()
+
+	// 等待所有 goroutine 退出
+	timeout := time.After(1 * time.Second)
+	count := 0
+	for count < 2 {
+		select {
+		case <-done:
+			count++
+		case <-timeout:
+			t.Fatal("Timeout waiting for goroutines to exit")
+		}
+	}
+}
