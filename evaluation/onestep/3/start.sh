f9a3f0d60265a209eb57901aa626c01100e06021
kimroniny <1301862177@qq.com>
2025-05-12 00:38:52 +0800

    update onestep/3/start.sh to support analysis

--- evaluation/onestep/3/start.sh
+++ evaluation/onestep/3/start.sh
@@ -17,13 +17,36 @@ echo ">>> >>> >>> start geth"
 screen -S session_geth_1 -dm ../SuperRunner-go-ethereum/build/bin/geth --dev --dev.period 3 --keystore ./node/keystore --allow-insecure-unlock --http --http.api eth,web3,net,miner,txpool,admin --ws --ws.api eth,web3,net --http.port 8545 --ws.port 8546
 screen -S session_geth_2 -dm ../SuperRunner-go-ethereum/build/bin/geth --dev --dev.period 3 --keystore ./node/keystore --allow-insecure-unlock --http --http.api eth,web3,net,miner,txpool,admin --ws --ws.api eth,web3,net --http.port 8555 --ws.port 8556
 screen -S session_geth_3 -dm ../SuperRunner-go-ethereum/build/bin/geth --dev --dev.period 3 --keystore ./node/keystore --allow-insecure-unlock --http --http.api eth,web3,net,miner,txpool,admin --ws --ws.api eth,web3,net --http.port 8565 --ws.port 8566
-sleep 2s
+sleep 3s
 check_session "session_geth_1"
+if [ $? -eq 0 ]; then
+    echo "Terminating script due to missing session: session_geth_1"
+    exit 1
+fi
 check_session "session_geth_2"
+if [ $? -eq 0 ]; then
+    echo "Terminating script due to missing session: session_geth_2"
+    exit 1
+fi
 check_session "session_geth_3"
+if [ $? -eq 0 ]; then
+    echo "Terminating script due to missing session: session_geth_3"
+    exit 1
+fi
 
 echo ">>> >>> >>> start metrics"
 screen -L -S session_metrics -dm ./metrics/run_metrics.sh
+sleep 2s
+check_session "session_metrics"
+if [ $? -eq 0 ]; then
+    echo "Terminating script due to missing session: session_metrics"
+    exit 1
+fi
+
+# echo ">>> >>> >>> start analysis"
+# screen -L -S session_analysis -dm ./analysis/run_analysis.sh
+# sleep 2s
+# check_session "session_analysis"
 
 echo ">>> >>> >>> start deploy"
 ./scripts/deploy.sh --config=evaluation/configs/config1.yaml
@@ -36,8 +59,20 @@ screen -L -S session_eICN_2 -dm go run main.go --config=evaluation/configs/confi
 screen -L -S session_eICN_3 -dm go run main.go --config=evaluation/configs/config3.yaml --log=logs/3.log --debug=$DEBUG
 sleep 3s
 check_session "session_eICN_1"
+if [ $? -eq 0 ]; then
+    echo "Terminating script due to missing session: session_eICN_1"
+    exit 1
+fi
 check_session "session_eICN_2"
+if [ $? -eq 0 ]; then
+    echo "Terminating script due to missing session: session_eICN_2"
+    exit 1
+fi
 check_session "session_eICN_3"
+if [ $? -eq 0 ]; then
+    echo "Terminating script due to missing session: session_eICN_3"
+    exit 1
+fi
 
 echo ">>> >>> >>> start regist"
 ./scripts/regist_eICN.sh --config=evaluation/configs/config1.yaml --target-config=evaluation/configs/config2.yaml
@@ -45,8 +80,8 @@ echo ">>> >>> >>> start regist"
 ./scripts/regist_eICN.sh --config=evaluation/configs/config2.yaml --target-config=evaluation/configs/config1.yaml
 ./scripts/regist_eICN.sh --config=evaluation/configs/config3.yaml --target-config=evaluation/configs/config1.yaml
 
-echo ">>> >>> >>> start cross send"
-sleep 3s
-./scripts/cross_send.sh --chain-ids="2,3" --value="100" --config=evaluation/configs/config1.yaml
+# echo ">>> >>> >>> start cross send"
+# sleep 3s
+# ./scripts/cross_send.sh --chain-ids="2,3" --value="100" --config=evaluation/configs/config1.yaml
 
 echo ">>> >>> >>> Finished!"
