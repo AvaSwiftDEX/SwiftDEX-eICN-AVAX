a18ca30429715a4a0b1b009418462c632c866299
kimroniny <1301862177@qq.com>
2025-05-18 22:28:09 +0800

    update shell

--- analysis/run_analysis.sh
+++ analysis/run_analysis.sh
@@ -4,4 +4,8 @@
 total_number=$1
 echo "total number: $total_number"
 
-go run analysis/*.go --metrics-server-url=127.0.0.1:8090 --total-number=$total_number
+# Store the identifier parameter
+identifier=$2
+echo "identifier: $identifier"
+
+go run analysis/*.go --metrics-server-url=127.0.0.1:8090 --total-number=$total_number --identifier=$identifier
