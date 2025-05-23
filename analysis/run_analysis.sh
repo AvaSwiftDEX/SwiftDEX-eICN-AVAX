400657569a1314fb604896e1a719945273832000
kimroniny <1301862177@qq.com>
2025-05-23 22:00:48 +0800

    support extract

--- analysis/run_analysis.sh
+++ analysis/run_analysis.sh
@@ -8,4 +8,12 @@ echo "total number: $total_number"
 identifier=$2
 echo "identifier: $identifier"
 
-go run analysis/*.go --metrics-server-url=127.0.0.1:8090 --total-number=$total_number --identifier=$identifier
+# Store the config file paths
+config_files=$3
+echo "config files: $config_files"
+
+# Store the extract parameter
+extract=$4
+echo "extract: $extract"
+
+go run analysis/*.go --metrics-server-url=127.0.0.1:8090 --total-number=$total_number --identifier=$identifier --config-files=$config_files --extract=$extract
