03eee0b2ada2adaf60a9979298cc4823ac91583d
kimroniny <1301862177@qq.com>
2025-05-27 20:29:03 +0800

    update configs

--- evaluation/cfg2pc/analysis.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/c
-
-./analysis/run_analysis.sh 50 test $SCRIPT_DIR/config1.yaml,$SCRIPT_DIR/config2.yaml,$SCRIPT_DIR/config3.yaml false
