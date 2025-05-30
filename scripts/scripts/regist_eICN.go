f8b7f6590d59a930d036db074e77ea330e825d6c
kimroniny <1301862177@qq.com>
2025-05-30 09:48:56 +0800

    update because of base_url

--- scripts/scripts/regist_eICN.go
+++ scripts/scripts/regist_eICN.go
@@ -1,14 +1,16 @@
 package scripts
 
 import (
+	"strconv"
+
 	"github.com/kimroniny/SuperRunner-eICN-eth2/client"
 	"github.com/kimroniny/SuperRunner-eICN-eth2/config"
 )
 
 func RegistEICN(sourceConfig *config.Config, targetConfig *config.Config) error {
 	transmitterClient := client.NewTransmitterClient(nil)
-	sourceTransmitterURL := sourceConfig.HTTP.BaseURL
-	targetTransmitterURL := targetConfig.HTTP.BaseURL
+	sourceTransmitterURL := "http://" + sourceConfig.HTTP.Host + ":" + strconv.Itoa(int(sourceConfig.HTTP.Port))
+	targetTransmitterURL := "http://" + targetConfig.HTTP.Host + ":" + strconv.Itoa(int(targetConfig.HTTP.Port))
 	sourceChainID := sourceConfig.Chain.ID
 
 	err := transmitterClient.RegisterEICN(sourceTransmitterURL, sourceChainID, targetTransmitterURL)
